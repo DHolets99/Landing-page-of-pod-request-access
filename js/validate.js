2b233411018f17f5fab224a5693b3a4b7e0fa49b
DHolets99 <dholets99@gmail.com>
2022-08-25 20:57:00 +0300

    25.08 20.56

---
+++ js/validate.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('.btn').on("click", validate)
+
+    const validateEmail = (email) => {
+        const re = /([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+        return re.test(email);
+    }
+
+    const sendForm = () => {
+        $('.error').css({color: "#54E6AF"});
+        $('.error').text('Email sending').fadeIn();
+    }
+
+    const showError = () => {
+        $('.error').css({color: "#FB3E3E"});
+        $('.error').text('Oops! Please check your email').fadeIn();
+    }
+
+    function validate(e) {
+        e.preventDefault();
+
+        let email = $('.email').val();
+
+        if (validateEmail(email)) {
+            sendForm();
+        } else {
+            showError();
+        }
+    }
+})
